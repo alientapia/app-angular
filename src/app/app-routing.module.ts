0d2821c6178cae67ff64061306c1fd9c7cbcdcdc
Alien Tapia Salvador <alientapia33@gmail.com>
2019-03-23 15:12:51 -0300

    agregando pag inicio nosotros y contactos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './components/inicio/inicio.component';
+import {NosotrosComponent} from './components/nosotros/nosotros.component';
+import {ContactosComponent} from './components/contactos/contactos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contactos', component: ContactosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
